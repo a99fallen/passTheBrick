c12b3228d66377361f42bec92bd71ce366b012f6
a99fallen <fallen_a99@o2.pl>
2021-11-30 23:16:08 +0100

    project init

---
+++ src/main/java/com/passthebrick/PassthebrickApplication.java
@@ -0,0 +1,13 @@
+package com.passthebrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassthebrickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassthebrickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/passthebrick/PassthebrickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passthebrick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassthebrickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
